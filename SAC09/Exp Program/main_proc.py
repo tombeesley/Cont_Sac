1003ba9224d8263dcfabd52612d1a0305be55c93
Tom Beesley <t.beesley@lancaster.ac.uk>
2022-02-01 21:06:51 +0000

    update main

--- SAC09/Exp Program/main_proc.py
+++ SAC09/Exp Program/main_proc.py
@@ -1,8 +1,10 @@
-from psychopy import visual, core, gui
-from psychopy.event import Mouse
+from psychopy import visual, core, gui, event
 import numpy as np
 import csv
 import os.path
+import random
+
+random.seed() # random seed based on clock
 
 winWidth = 960; winHeight = 540
 win = visual.Window(
@@ -36,12 +38,14 @@ win = visual.Window(
 #    print('Setup cancelled')
 #    core.quit()
 
+# design of experiment
+# cue 1 | cue 2 | cue_correct (L/R) | instruction (C/S)
 design = np.array([\
-[1,3],[2,3],[1,4],[2,4],\
-[3,1],[3,2],[4,1],[4,2]], dtype = int)
+[1,3,1],[2,3,1],[1,4,1],[2,4,1],\
+[3,1,2],[3,2,2],[4,1,2],[4,2,2]], dtype = int)
 
-blocks = 2
-trialArr = np.empty([0,3], dtype = int)
+blocks = 1
+trialArr = np.empty([0,4], dtype = int)
 for b in range(0,blocks):
     np.random.shuffle(design) # randomise the trial order
     newBlock = np.append(design,np.ones((8,1),dtype=int)*(b+1), axis = 1) # add block numbers
@@ -56,31 +60,74 @@ stimCols = np.insert(stimCols,0,0,axis=0)
 
 # create the stimuli
 fixation = visual.Circle(\
-win=win, units = "pix", pos=[0,0], radius = 20, fillColorSpace = 'rgb255', fillColor = [120,120,120], lineWidth = 0)
+win=win, pos=[0,0], radius = 20, fillColorSpace = 'rgb255', fillColor = [120,120,120], lineWidth = 0)
 
 L_cue = visual.Circle(\
-win=win, units = "pix", pos=[-200,0], radius = 100, fillColorSpace = 'rgb255', fillColor = [0,0,0], lineWidth = 0)
+win=win, pos=[-200,0], radius = 100, fillColorSpace = 'rgb255', fillColor = [0,0,0], lineWidth = 0)
 
 R_cue = visual.Circle(\
-win=win, units = "pix", pos=[200,0], radius = 100, fillColorSpace = 'rgb255', fillColor = [0,0,0], lineWidth = 0)
+win=win, pos=[200,0], radius = 100, fillColorSpace = 'rgb255', fillColor = [0,0,0], lineWidth = 0)
+
+R_text_background = visual.Rect(\
+win=win, pos=[200,0], size = 30, fillColorSpace = 'rgb255', fillColor = [255,255,255])
+
+L_text_background = visual.Rect(\
+win=win, pos=[-200,0], size = 30, fillColorSpace = 'rgb255', fillColor = [255,255,255])
+
+R_text = visual.TextStim(\
+win=win, pos=[200,0], colorSpace = 'rgb255', color = (220,220,220))
+
+L_text = visual.TextStim(\
+win=win, pos=[-200,0], colorSpace = 'rgb255', color = (220,220,220))
+
+letter_keys = np.array(['A', 'Z', 'K', 'M'], dtype = object) # response prompts/keys 
+
+fb_text = visual.TextStim(\
+win=win, pos=[0,0], colorSpace = 'rgb255', color = (0,0,0))
 
 for t in range(0,trialArr.shape[0]):
+    
+    np.random.shuffle(letter_keys) # shuffle the response keys
+    
     # draw fixation
     fixation.draw()
+    win.flip()
+    core.wait(.5)
     
+    if trialArr[t][2] == 1: # if left cue is P
+        corResp = letter_keys[0] 
+    elif trialArr[t][2] == 2: # if right cue is P
+        corResp = letter_keys[1] 
     
-
     # draw left stimulus
-    L_cue.fillColor = stimCols[trialArr[t][0]]
+    L_cue.fillColor = stimCols[trialArr[t][0]] # get colour from trial structure
     L_cue.draw()
+    L_text_background.draw()
+    L_text.text = letter_keys[0]
+    L_text.draw()
     # draw right stimulus
-    R_cue.fillColor = stimCols[trialArr[t][1]]
+    R_cue.fillColor = stimCols[trialArr[t][1]] # get colour from trial structure
     R_cue.draw()
+    R_text_background.draw()
+    R_text.text = letter_keys[1]
+    R_text.draw()
 
     win.flip()
-    core.wait(1) # stimulus duration time
+#    core.wait(.5) # stimulus duration time
+#    win.flip()
+#    core.wait(0.2) # ITI
+    
+    #key = event.waitKeys(maxWait=5,keyList=letter_keys[0:2]) # waits for either an r or b key to be pressed. If 5 seconds pass, then it moves on without a response
+    key = event.waitKeys(maxWait=5,keyList=['a','z','K', 'M'])
+    if key == corResp:
+        fb_text.text = "Correct"
+    else:
+        fb_text.text = "Incorrect"
+    
+    fb_text.draw()
     win.flip()
-    core.wait(0.2) # ITI
+    core.wait(1)
+    
 
 win.flip()
 core.wait(1)
